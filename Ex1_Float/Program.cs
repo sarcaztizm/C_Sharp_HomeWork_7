077357bf555ba8f074d64b9ba51d98e9cd65fefb
Eric <ericismag@outlook.com>
2022-09-27 12:32:31 +0300

    First Task

---
+++ Ex1_Float/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задайте двумерный массив размером MxN,
+// заполненный случайными вещественными числами
+
+Console.Clear();
+
+Console.Clear();
+Console.WriteLine("Введите размерность массива:");
+Console.Write("Количество строк ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.Write("Количество столбцов  ");
+int M = Convert.ToInt32(Console.ReadLine());
+
+double [,] MyMatrix = new double[N, M];
+
+void InputMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().NextDouble() + new Random().Next(1,11);
+        }
+    }
+}
+
+void PrintMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]}  ");
+        }
+        Console.WriteLine();
+    }
+}
+
+InputMatrix(MyMatrix);
+PrintMatrix(MyMatrix);
